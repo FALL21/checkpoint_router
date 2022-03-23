d8ad0e8f73886a6cd23f53266d2ef3a20db71a71
mameboufall21@gmail.com <YOUR EMAIL>
2022-03-23 22:31:59 +0000

    checkoint_router

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MovieApp from './component/MovieApp';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <MovieApp/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/RatingStart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Rating } from 'react-simple-star-rating'
+
+export default function RatingStart() {
+  return (
+    <div className='App'>
+      <Rating/>
+    </div>
+  )
+}
